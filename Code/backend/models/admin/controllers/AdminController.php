c2bf981ad9691c9df45e9d7f8ecfee96078f5e7f
upupjie <15202980611@163.com>
2017-10-26 17:48:25 +0800

    edit

---
+++ Code/backend/models/admin/controllers/AdminController.php
@@ -0,0 +1,72 @@
+<?php
+namespace backend\models\admin\controllers;
+
+use Yii;
+use backend\controllers\BaseController;
+use backend\models\admin\model\AdminModel;
+use common\helpers\FuncHelper;
+
+/**
+ * Site controller
+ */
+class AdminController extends BaseController
+{
+
+
+    /**
+     * 管理员首页
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionAjaxgetindexlist()
+    {
+        $searchModel = new AdminModel();
+
+        if (Yii::$app->request->isAjax) {
+            $data = $searchModel->getAdminAll(Yii::$app->request->queryParams);
+            return $this->resAjax($data);
+        }
+    }
+
+    public function actionEdit()
+    {
+        return $this->render('edit');
+    }
+
+    public function actionAjaxsave()
+    {
+        $model = new AdminModel();
+
+        if (Yii::$app->request->isPost) {
+            $data = Yii::$app->request->post();
+
+            if ($model->load($data, '') && $model->validate()) {
+
+                $data['reg_time'] = time();
+                $data['reg_ip'] = ip2long(Yii::$app->request->getUserIP());
+                $data['last_login_time'] = 0;
+                $data['last_login_ip'] = ip2long('127.0.0.1');
+                $data['update_time'] = time();
+
+                $model->generateAuthKey();
+                $model->setPassword($data['password']);
+
+                if ($model->save()) {
+                    return $this->resAjax(['code' => 0, 'err' => '操作成功！']);
+                } else {
+                    return $this->resAjax($model->resLoginCode());
+                }
+            } else {
+                return $this->resAjax($model->resLoginCode());
+            }
+        }
+    }
+
+
+
+}
